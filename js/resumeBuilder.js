650da57f931f8b93f428833921c002fab25a0de7
tomascain <tomascaindh5@crazespaces.pw>
2015-09-08 18:04:31 +0530

    Revert "Revert "adds name and role""
    This reverts commit df5c536c5662fe9f0f872f541955377d76b12322.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,12 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+
+ var name = "Thomas Cain";
+ var role = "Web Developer";
+ var formattedName = HTMLheaderName.replace("%data%", name);
+ var formattedRole = HTMLheaderRole.replace("%data%", role);
+ $("#header").prepend(formattedRole);
+ $("#header").prepend(formattedName);
+
